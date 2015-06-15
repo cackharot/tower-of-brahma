324338b111fab3f1fc36fee961f9556f0d9ae8dc
thirus <thiruvarasan.shanmugam@gmail.com>
2015-06-15 17:13:30 +0530

    added simple 2^n-1 computation

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,10 @@
-main ::  IO ()
+import System.Environment
+
 main = do
-    print "Test"
+    a <- getArgs
+    putStr "Number of turns to finish is "
+    let n = fromInteger $ read $ head a in 
+        print $ turns n
     return ()
+    where
+        turns n = (2^n) - 1
